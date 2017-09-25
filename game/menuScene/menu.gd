8fc29303b1770ddd22a98de2a86540b96c16ced7
Mijo Gracanin <mgracanin@rhapsody.com>
2017-09-25 17:11:03 +0200

    Create GoatRescue project

---
+++ game/menuScene/menu.gd
@@ -0,0 +1,19 @@
+extends Panel
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _on_normalButton_pressed():
+	get_tree().change_scene("res://level1Scene/level1.tscn")
+
+func _on_hardcoreButton_pressed():
+	pass # replace with function body
+
+func _on_creditsButton_pressed():
+	pass # replace with function body
